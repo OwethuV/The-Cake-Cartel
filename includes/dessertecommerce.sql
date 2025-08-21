f459e5167feec0736dbb3f8feb614ddd97881d7b
Owethu Valantiya <outisvalantiya@gmail.com>
2025-08-21 10:13:32 +0200

    Update dessertecommerce.sql

--- includes/dessertecommerce.sql
+++ includes/dessertecommerce.sql
@@ -6,6 +6,7 @@ USE DessertECommerce;
 CREATE TABLE IF NOT EXISTS USERS (
     userId INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    profile_picture VARCHAR(255) DEFAULT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     cell VARCHAR(20),
     address TEXT,
@@ -48,7 +49,7 @@ CREATE TABLE IF NOT EXISTS ORDERS (
     status VARCHAR(50) NOT NULL DEFAULT 'Pending',
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (cartId) REFERENCES CART(cartId) ON DELETE CASCADE
+    FOREIGN KEY (userId) REFERENCES USERS(userId) ON DELETE CASCADE
 );
 
 -- Create ORDER_ITEMS table
@@ -119,11 +120,3 @@ But the true magic happens when they are fresh from the oven, drizzled with a th
 ('Chocolate Hazelnut Éclairs', 'https://raw.githubusercontent.com/OwethuV/product_pictures/main/eclairs.webp', 199.99, 'Unleash your inner connoisseur with our exquisite Chocolate Hazelnut Éclairs.
 This is a true French classic, elevated to a new level of pure indulgence. We begin with a delicate, airy choux pastry shell, baked to a perfect golden crispness. Each éclair is then generously filled with a rich, velvety chocolate hazelnut cream, a blend so smooth and decadent its like a dream.
 The finishing touch is a luxurious dark chocolate ganache, which coats the top with a glossy, deep cocoa shine. A final sprinkle of crunchy, toasted hazelnuts adds a delightful texture and a nutty, aromatic finish. Its a symphony of textures and flavors—from the crisp pastry and creamy filling to the rich chocolate and satisfying crunch—that will transport you /straight to a Parisian patisserie.', 'Hazelnut');
-
-SELECT o.orderId, o.deliveryPrice, o.totalPrice, o.status, o.createdAt,
-                               oi.productId, p.productName, p.productImg, oi.quantity, oi.price AS itemTotalPrice
-                        FROM ORDERS o
-                        JOIN ORDER_ITEMS oi ON o.orderId = oi.orderId
-                        JOIN PRODUCTS p ON oi.productId = p.productId
-                        WHERE o.userId = 4
-                        ORDER BY o.createdAt DESC;
