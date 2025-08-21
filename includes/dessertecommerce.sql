fd256b1c492a2354c39f7bc1e41f45a1aebe2059
Owethu Valantiya <outisvalantiya@gmail.com>
2025-08-21 10:27:59 +0200

    Update dessertecommerce.sql

--- includes/dessertecommerce.sql
+++ includes/dessertecommerce.sql
@@ -64,6 +64,15 @@ CREATE TABLE IF NOT EXISTS ORDER_ITEMS (
     FOREIGN KEY (productId) REFERENCES PRODUCTS(productId) ON DELETE CASCADE
 );
 
+-- Create password_resets table
+CREATE TABLE IF NOT EXISTS password_resets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    expires INT NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Inserting products into the PRODUCTS table
 INSERT INTO PRODUCTS (productName, productImg, price, description, flavor) VALUES
 ('Chocolate Fudge Cake', 'https://raw.githubusercontent.com/OwethuV/product_pictures/main/chocolate-fudge.webp', 249.99, 'Indulge in a slice of pure decadence with our chocolate fudge cake.
@@ -120,3 +129,4 @@ But the true magic happens when they are fresh from the oven, drizzled with a th
 ('Chocolate Hazelnut Éclairs', 'https://raw.githubusercontent.com/OwethuV/product_pictures/main/eclairs.webp', 199.99, 'Unleash your inner connoisseur with our exquisite Chocolate Hazelnut Éclairs.
 This is a true French classic, elevated to a new level of pure indulgence. We begin with a delicate, airy choux pastry shell, baked to a perfect golden crispness. Each éclair is then generously filled with a rich, velvety chocolate hazelnut cream, a blend so smooth and decadent its like a dream.
 The finishing touch is a luxurious dark chocolate ganache, which coats the top with a glossy, deep cocoa shine. A final sprinkle of crunchy, toasted hazelnuts adds a delightful texture and a nutty, aromatic finish. Its a symphony of textures and flavors—from the crisp pastry and creamy filling to the rich chocolate and satisfying crunch—that will transport you /straight to a Parisian patisserie.', 'Hazelnut');
+
