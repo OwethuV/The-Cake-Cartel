779d38876167397edc12cc3ffe33d099d4914e06
Owethu Valantiya <outisvalantiya@gmail.com>
2025-08-24 11:12:55 +0200

    Delete cancel.php

--- cancel.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-echo "<h1>Payment Cancelled</h1>";
-echo "<p>You cancelled the payment. Your order has not been processed.</p>";
-echo "<a href='/cart.php'>Return to cart</a>";
