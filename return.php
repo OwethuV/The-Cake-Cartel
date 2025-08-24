1ffb9235bb3bbcdf834dd0e786c13dbeee547108
Owethu Valantiya <outisvalantiya@gmail.com>
2025-08-24 11:12:29 +0200

    Delete return.php

--- return.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// You can show order confirmation here or redirect to a thank you page
-
-echo "<h1>Thank you for your payment!</h1>";
-echo "<p>Your order has been placed successfully and payment processed.</p>";
-echo "<a href='/'>Return to homepage</a>";
